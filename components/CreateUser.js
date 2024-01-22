a35531af5082ec1751d59fae44999af45acd4902
Abigail-DePriest <abby.depriest@gmail.com>
2024-01-21 21:46:29 -0600

    Finished Profile and Signout

--- components/CreateUser.js
+++ components/CreateUser.js
@@ -1,31 +1,15 @@
-/* import React from 'react';
-import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
+/* eslint-disable @next/next/no-img-element */
+import React from 'react';
+import { useAuth } from '../utils/context/authContext';
 
-function Profile() {
+export default function CreateUser() {
   const { user } = useAuth();
-
-  return <div>profile here</div>;
-
-  const signOut = () => {
-    firebase.auth().signOut();
-     };
-
   return (
-     <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-    style={{
-      height: '90vh',
-      padding: '30px',
-      maxWidth: '400px',
-      margin: '0 auto',
-      }}
-  >
-    <Button type="button" size="lg" className="copy-btn" onClick={signOut}>
-      Sign In
-    </Button>
-  </div>
+    <>
+      <img src={user.photoURL} alt="Abby DePriest" style={{ width: '140px', borderRadius: '60%' }} />
+      <h1>Name: {user.displayName}</h1>
+      <h1>Email: {user.email} </h1>
+      <h1>Last Login {user.metadata?.lastSignInTime}</h1>
+    </>
   );
 }
-
-export default { SignOut, Profile }; */
